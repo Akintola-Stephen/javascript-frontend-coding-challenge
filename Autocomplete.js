72adbb7bb48a3293b466b95a021d0c7001430842
AKINTOLA STEPHEN IYANU <sakintola351@gmail.com>
2021-02-17 11:32:17 +0100

    still working on it

--- Autocomplete.js
+++ Autocomplete.js
@@ -6,6 +6,65 @@ export default class Autocomplete {
     this.init();
   }
 
+  init() {
+    // Build query input
+    this.inputEl = this.createQueryInputEl();
+    this.rootEl.appendChild(this.inputEl);
+
+    // Build results dropdown
+    this.listEl = document.createElement("ul");
+    Object.assign(this.listEl, { className: "results border-no" });
+    this.rootEl.appendChild(this.listEl);
+
+    //initialize list index as a reference for arrow keys
+    this.listIndex = -1;
+
+    //timeout variable allows slight delay in api call to account for continuous typing with debounce function
+    this.timeout = null;
+
+    //clear info when searching another input box
+    this.inputEl.addEventListener("blur", () => {
+      this.inputEl.value = "";
+      // document.getElementById('select-text').innerHTML = '';
+      // let selectImg = document.getElementById('select-img');
+      // selectImg.src = '';
+      // selectImg.alt = '';
+    });
+
+    //add event listener for arrow keys to the root element
+    this.rootEl.addEventListener("keydown", this.onKeyboardEvent);
+    //Event listener callback moved to onSelectElement to accomodate both click and Enter events
+    this.rootEl.addEventListener("click", this.onSelectElement);
+
+    //variable updates based on the current input field - used for testing only
+    this.currentQuery = "";
+  }
+
+
+  createQueryInputEl() {
+    const inputEl = document.createElement('input');
+    Object.assign(inputEl, {
+      type: 'search',
+      name: 'query',
+      autocomplete: 'off'
+    });
+    inputEl.addEventListener('input', event => {
+      this.currentQuery = event.target.value;
+
+      //check whether there is an onInputChange function to determine whether there is an API call or not
+      if(typeof(this.options.onInputChange) !== 'function') {
+        this.onQueryChange(event.target.value);
+      } else {
+        //reduce the number of api calls with debounce
+        this.debounce(() => this.onQueryChange(event.target.value));
+      }   
+    });
+      
+    return inputEl;
+  }
+
+
+
   onQueryChange(query) {
     // Get data for the dropdown
     let results = this.getResults(query, this.options.data);
@@ -29,23 +88,23 @@ export default class Autocomplete {
   }
 
   updateDropdown(results) {
-    this.listEl.innerHTML = '';
+    this.listEl.innerHTML = "";
     this.listEl.appendChild(this.createResultsEl(results));
   }
 
   createResultsEl(results) {
     const fragment = document.createDocumentFragment();
     results.forEach((result) => {
-      const el = document.createElement('li');
+      const el = document.createElement("li");
       Object.assign(el, {
-        className: 'result',
+        className: "result",
         textContent: result.text,
       });
 
       // Pass the value to the onSelect callback
-      el.addEventListener('click', (event) => {
+      el.addEventListener("click", (event) => {
         const { onSelect } = this.options;
-        if (typeof onSelect === 'function') onSelect(result.value);
+        if (typeof onSelect === "function") onSelect(result.value);
       });
 
       fragment.appendChild(el);
@@ -54,15 +113,16 @@ export default class Autocomplete {
   }
 
   createQueryInputEl() {
-    const inputEl = document.createElement('input');
+    const inputEl = document.createElement("input");
     Object.assign(inputEl, {
-      type: 'search',
-      name: 'query',
-      autocomplete: 'off',
+      type: "search",
+      name: "query",
+      autocomplete: "off",
     });
 
-    inputEl.addEventListener('input', event =>
-      this.onQueryChange(event.target.value));
+    inputEl.addEventListener("input", (event) =>
+      this.onQueryChange(event.target.value)
+    );
 
     return inputEl;
   }
@@ -70,11 +130,11 @@ export default class Autocomplete {
   init() {
     // Build query input
     this.inputEl = this.createQueryInputEl();
-    this.rootEl.appendChild(this.inputEl)
+    this.rootEl.appendChild(this.inputEl);
 
     // Build results dropdown
-    this.listEl = document.createElement('ul');
-    Object.assign(this.listEl, { className: 'results' });
+    this.listEl = document.createElement("ul");
+    Object.assign(this.listEl, { className: "results" });
     this.rootEl.appendChild(this.listEl);
   }
 }
--- index.js
+++ index.js
@@ -15,10 +15,56 @@ new Autocomplete(document.getElementById('state'), {
   },
 });
 
+//GitHub Users
+new Autocomplete(document.getElementById('gh-user'), {
+  url: 'https://api.github.com/search/users',
+  querySymbol: 'q=',
+  queryEndParams: '+in:login', //specific to GitHub's API - search just login property
+  perPageKey: '&per_page=',
+  onInputChange: (initialData) => {
+    let userLogins = initialData.items.map(user => { 
+      return ({
+        text: user.login, 
+        img: user.avatar_url + '.png' 
+        })
+      });
+    return userLogins;
+  },
+  onSelect: (ghUserId) => {
+    console.log('selected github user id:', ghUserId.textContent);
+    
+  },
+});
 
-// Github Users
-// new Autocomplete(document.getElementById('gh-user'), {
-//   onSelect: (ghUserId) => {
-//     console.log('selected github user id:', ghUserId);
-//   },
-// });
+//Breweries
+new Autocomplete(document.getElementById('brewery'), {
+  url: 'https://api.openbrewerydb.org/breweries/autocomplete',
+  querySymbol: 'query=',
+  perPageKey: '&per_page=',
+  onInputChange: (initialData) => {
+    return initialData.map(brew => ({ text: brew.name}));   
+  },
+  onSelect: (brewery) => {
+    console.log('selected brewery name: ', brewery.textContent);
+  },
+});
+
+//NASA images
+//there is not a clear way to specify the number of results per page
+new Autocomplete(document.getElementById('nasa'), {
+  url: 'https://images-api.nasa.gov/search',
+  querySymbol: 'title=',
+  queryEndParams: '&media_type=image',
+  onInputChange: (initialData) => {
+    let nasaImgs = initialData.collection.items.map(image => {
+      return ({ 
+        text: image.data[0].title,
+        img: image.links[0].href
+      })
+    });
+    return nasaImgs;
+  },
+  onSelect: (nasaImg) => {
+    console.log('selected image title: ', nasaImg.textContent);
+  },
+});
